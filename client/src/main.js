9f7fe997b3ecc763fdbd665181fff0eee600c5e4
Rosie Allison-Corio <rosievespa@gmail.com>
2025-01-30 11:04:08 +0000

    add function to fetch API data, add function to generate elements for data

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 const myForm = document.getElementById('myForm')
 const nameInput = document.getElementById('nameInput')
 const commentInput = document.getElementById('commentInput')
+const commentContainer = document.getElementById('commentContainer')
 
 myForm.addEventListener('submit', handleSubmit)
 
@@ -19,8 +20,30 @@ function handleSubmit (event) {
   })
 }
 
+async function fetchCommentData() {
+  const response = await fetch('https://week-3-project-server.onrender.com/guestbook')
+  const data = await response.json()
+  generateCommentBox(data)
+}
 
-/*async function getMessages() {
-  const data = await fetch('https://week-3-project-server.onrender.com');
-  const response = await data.json
-}*/
+fetchShopData()
+
+function generateCommentBox(dataToRender) {
+  for (let i = 0; i < dataToRender.length; i++) {
+    const eachCommentElem = document.createElement('div');
+    containerElem.setAttribute('class', 'shopItem')
+    const nameElem = document.createElement('p')
+    const commentElem = document.createElement('p')
+    
+
+    nameElem.innerText = dataToRender[i].name
+    commentElem.innerText = dataToRender[i].comment
+    
+
+    
+    eachCommentElem.appendChild(nameElem)
+    eachCommentElem.appendChild(commentElem)
+    
+    commentContainer.appendChild(eachCommentElemElem)
+  }
+}
